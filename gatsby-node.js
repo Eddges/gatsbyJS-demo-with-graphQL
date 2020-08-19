a51164677e004ffe254bffd516d409c827f26518
Eddges <shivamshekhar.i7@gmail.com>
2020-08-19 22:34:45 +0530

    slug added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,15 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+module.exports.onCreateNode = ({node, actions}) => {
+    const { createNodeField } = actions
 
-// const path = require('path')
-
-// module.exports.onCreateNode = ({node, actions}) => {
-//     const { createNodeField } = actions
-
-//     if(node.internal.type==='MarkdownRemark'){
-//         const slug = path.basename(node.fileAbsolutePath, '.md')
-//         console.log('@@@@@@@@@@@@@@@@@@', slug)
-
-//         createNodeField({
-//             node, 
-//             name : 'slug',
-//             value : slug
-//         })
-//     }
-// }
+    if(node.internal.type === 'MarkdownRemark') {
+        const slug = path.basename(node.fileAbsolutePath, '.md')
+        console.log('@@@@@@@@@@@@@@@', slug)
+        createNodeField({
+            node,
+            name : 'slug',
+            value : slug
+        })
+    }
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Blog = (props) => {
+    return(
+        <div>
+            <Layout>
+                This is blog template
+            </Layout>
+        </div>
+    )
+}
