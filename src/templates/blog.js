9e03d698d6dd588ce86a5103c4587811b91f4848
Eddges <shivamshekhar.i7@gmail.com>
2020-08-20 18:48:49 +0530

    implemented blog generation with markdown

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 import classes from '../styles/blog/blog.module.css'
+import { graphql } from 'gatsby'
+
+
+export const query = graphql`
+query (
+    $slug :String
+  ){
+    markdownRemark(
+      fields :{
+        slug:{
+          eq :$slug
+        }
+      }
+    ){
+        frontmatter{
+        title
+        date
+      }
+        html
+    }
+  }
+`
 
 const Blog = (props) => {
     return(
             <Layout>
                 <div className={classes.Container}>
-                    <span>This is a blog template</span>
+                    <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+                    <p>{props.data.markdownRemark.frontmatter.date}</p>
+                    <div dangerouslySetInnerHTML={{__html : props.data.markdownRemark.html}} >
+                    </div>
                 </div>
             </Layout>
     )
